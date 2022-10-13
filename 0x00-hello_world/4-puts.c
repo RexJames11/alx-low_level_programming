00e584701d1598c8b02e089fb6006556264c1126
Obi_Ambrose <obiambroserex@gmail,com>
2022-10-13 13:51:01 -0700

    Use the printf() function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,12 +5,11 @@
  *
  * Description: 'Using the puts() function'
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
-
 	return (0);
 }
