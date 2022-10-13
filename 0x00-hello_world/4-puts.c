11a118a9edf283beec86b8c872e5348cf121d04f
Obi_Ambrose <obiambroserex@gmail,com>
2022-10-13 12:48:12 -0700

    Use the puts() function to print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: 'Using the puts() function'
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	char build[] = "Programming is like building a multilingual puzzle\n";
 
 	puts(build);
+
 	return (0);
 }
