c8a8efd19f8618740abd8f7979cd6bfc4d7b1118
Obi_Ambrose <obiambroserex@gmail,com>
2022-10-13 13:17:17 -0700

    Use the puts() function to print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-	char build[] = "Programming is like building a multilingual puzzle\n";
+	char build[] = "\"Programming is like building a multilingual puzzle\n";
 
 	puts(build);
 
